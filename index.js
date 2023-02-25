c417e776964414c25151531c025c665cd016da96
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-02-25 23:01:35 +0200

    complete tesk1 js

---
+++ index.js
@@ -0,0 +1,19 @@
+const a = +prompt("Введіть число:");
+const b = +prompt("Введіть друге число:");
+
+const sum = a + b;
+const difference = a - b;
+const multiply = a * b;
+const division = a / b;
+
+const result =
+  "сума: " +
+  sum +
+  "\nрізниця: " +
+  difference +
+  "\nдобуток: " +
+  multiply +
+  "\nчастка: " +
+  division;
+
+alert(result);
