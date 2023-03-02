b517aa96e4bd2552e730ab9a73b1aaf188882db3
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-02 22:14:49 +0200

    complete task2

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const a = +prompt("Введіть число:");
+if (a == "") {
+  alert("Error: Немає данних!");
+}
 const b = +prompt("Введіть друге число:");
+if (b == "") {
+  alert("Error: Немає данних!");
+}
 
-const sum = a + b;
-const difference = a - b;
-const multiply = a * b;
-const division = a / b;
-
-const result =
-  "сума: " +
-  sum +
-  "\nрізниця: " +
-  difference +
-  "\nдобуток: " +
-  multiply +
-  "\nчастка: " +
-  division;
-
-alert(result);
+if (b === 0) {
+  alert("Error: Ділення на нуль неможливе.");
+} else if (a < b) {
+  const confirmResult = confirm("Ви впевнені, що хочете продовжити операцію?");
+  if (confirmResult) {
+    const difference = a - b;
+    alert("Різниця: " + difference);
+  }
+} else {
+  const sum = a + b;
+  const difference = a - b;
+  const multiply = a * b;
+  const division = a / b;
+  const result =
+    "сума: " +
+    sum +
+    "\nрізниця: " +
+    difference +
+    "\nдобуток: " +
+    multiply +
+    "\nчастка: " +
+    division;
+  alert(result);
+}
